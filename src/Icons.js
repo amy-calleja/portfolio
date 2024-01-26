75f9dfa2588fafd114f3512f2645217b6a2e0fff
AmyCalleja <91883080+amy-calleja@users.noreply.github.com>
2024-01-26 17:50:00 +0100

    titles of stack icons, begin change color function

--- src/Icons.js
+++ src/Icons.js
@@ -2,17 +2,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReact, faSquareJs, faHtml5, faCss3Alt, faGithub, faBootstrap, faNpm } from '@fortawesome/free-brands-svg-icons'
 
 export default function Icons() {
+
+    // when hovering, will return random color
+    const dColors = [
+    '#bd93f9',
+    '#ff79c6',
+    '#f8f8f2',
+    '#50fa7b',
+    '#8be9fd',
+    '#f1fa8c',
+    '#ffb86c',
+    '#ff5555'
+    ]
+    const dracularColor = dColors[Math.floor(Math.random() * 15)]
+
+    function changeColor(){
+        return dracularColor
+    }
+
     return(
     <div className="tech m-5 mx-3">
     <div className='comment'>// My Stack </div>
        <span>
-          <FontAwesomeIcon icon={faReact} className="icons" />
-          <FontAwesomeIcon icon={faSquareJs} className="icons" />
-           <FontAwesomeIcon icon={faHtml5} className="icons" />
-           <FontAwesomeIcon icon={faCss3Alt} className="icons" />
-           <FontAwesomeIcon icon={faGithub} className="icons" />
-           <FontAwesomeIcon icon={faBootstrap} className="icons" />
-           <FontAwesomeIcon icon={faNpm} className="icons" />
+          <FontAwesomeIcon  icon={faReact} className="icons" title='React.js'/>
+          <FontAwesomeIcon  icon={faSquareJs} className="icons" title='JavaScript'/>
+           <FontAwesomeIcon icon={faHtml5} className="icons" title='HTML5'/>
+           <FontAwesomeIcon icon={faCss3Alt} className="icons" title='CSS3'/>
+           <FontAwesomeIcon icon={faGithub} className="icons" title='Git / GitHub'/>
+           <FontAwesomeIcon icon={faBootstrap} className="icons" title='Bootstrap'/>
+           <FontAwesomeIcon icon={faNpm} className="icons" title='NPM and API'/>
        </span> 
    </div>
     );
