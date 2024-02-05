1df1eaefc2b7b7ade16bb36d223d82bb05d2de8f
AmyCalleja <91883080+amy-calleja@users.noreply.github.com>
2024-02-05 15:49:34 +0100

    random hover color function on icons

--- src/Icons.js
+++ src/Icons.js
@@ -1,36 +1,71 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReact, faSquareJs, faHtml5, faCss3Alt, faGithub, faBootstrap, faNpm } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+import { icon } from '@fortawesome/fontawesome-svg-core'
 
 export default function Icons() {
+    const [hover, setHover] = useState(false)
 
     // when hovering, will return random color
     const dColors = [
     '#bd93f9',
     '#ff79c6',
-    '#f8f8f2',
     '#50fa7b',
     '#8be9fd',
     '#f1fa8c',
     '#ffb86c',
     '#ff5555'
     ]
-    const dracularColor = dColors[Math.floor(Math.random() * 15)]
+    const dracularColor = dColors[Math.floor(Math.random() * 7)]
 
-    function changeColor(){
-        return dracularColor
-    }
+const icons = [
+    {
+        "key": "0",
+        "icon": faReact,
+        "title": "React.js"
+      },
+      {
+        "key": "1",
+        "icon": faSquareJs,
+        "title": "JavaScript"
+      },
+      {
+        "key": "2",
+        "icon": faHtml5,
+        "title": "HTML5"
+      },
+      {
+        "key": "3",
+        "icon": faCss3Alt,
+        "title": "CSS3"
+      },
+      {
+        "key": "4",
+        "icon": faGithub,
+        "title": "Git / GitHub"
+      },
+      {
+        "key": "5",
+        "icon": faBootstrap,
+        "title": "Bootstrap"
+      },
+      {
+        "key": "6",
+        "icon": faNpm,
+        "title": "NPM and API"
+      }
+    ]
 
     return(
     <div className="tech m-5 mx-3">
     <div className='comment'>// My Stack </div>
        <span>
-          <FontAwesomeIcon  icon={faReact} className="icons" title='React.js'/>
-          <FontAwesomeIcon  icon={faSquareJs} className="icons" title='JavaScript'/>
-           <FontAwesomeIcon icon={faHtml5} className="icons" title='HTML5'/>
-           <FontAwesomeIcon icon={faCss3Alt} className="icons" title='CSS3'/>
-           <FontAwesomeIcon icon={faGithub} className="icons" title='Git / GitHub'/>
-           <FontAwesomeIcon icon={faBootstrap} className="icons" title='Bootstrap'/>
-           <FontAwesomeIcon icon={faNpm} className="icons" title='NPM and API'/>
+       { icons.map((icon) => {
+        return(
+            <span>
+            <FontAwesomeIcon onMouseOver={()=>setHover(icon.key)} onMouseOut={()=>setHover(null)} style={hover === icon.key? {color: dracularColor} : null}  icon={icon.icon} className="icons" title={icon.title}/>
+            </span>); }
+       )} 
        </span> 
    </div>
     );
